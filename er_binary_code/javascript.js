adf244b96824f6274fc49b99d3bfe2c0da1f2a8a
Fabian.trllr <33369156+Exo-poulpe@users.noreply.github.com>
2018-11-06 11:41:11 +0100

    Refactor frontend + upgrade backend
    Use bootstrap 3.3.7 for frontend and upgrade backend for check soluce at interval

--- er_binary_code/javascript.js
+++ er_binary_code/javascript.js
@@ -1,74 +1,104 @@
+/*
+Auteur  : Lopes Miguel, Troller Fabian, Juling Guntram
+Date    : 2018.11.06
+Description : Code javascript cité des metiers
+ */
 const DEFAULT_CHAR = "_"; // caractere par defaut
-var listBin = [DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR,DEFAULT_CHAR];// initialisation de la table des solutions binaires avec les caracteres par defaut
+var listBin = [DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR, DEFAULT_CHAR];// initialisation de la table des solutions binaires avec les caracteres par defaut
 const LIMIT_BIN = 8; // taille limite de la liste binaire
 
 // Permet d'inserer la valeur dans la liste en decalant la liste
-function ListSetter(value){
-  listBin.push(value);
-  while (listBin.length > LIMIT_BIN)
-  {
-    listBin.shift(listBin);
-  }
-  UpdateView(listBin);
-  return listBin;
+function ListSetter(value) {
+    listBin.push(value);
+    while (listBin.length > LIMIT_BIN) {
+        listBin.shift(listBin);
+    }
+    UpdateView(listBin);
+    return listBin;
 }
 
 // Remet la liste par defaut
-function ResetArray(){
-  for (var i = 0; i < listBin.length; i++) {
-    listBin[i] = "_";
-  }
-  UpdateView(listBin);
+function ResetArray() {
+    for (var i = 0; i < listBin.length; i++) {
+        listBin[i] = "_";
+    }
+    UpdateView(listBin);
 }
 
 // Affiche les donnees sur la page
-function UpdateView(listBin)
-{
-  var bin = "";
-  var hex = [];
+function UpdateView(listBin) {
+    var bin = "";
+    var hex = [];
 
-  // remplace les caracteres par defaut par des 0
-  for (var i = 0; i < listBin.length; i++)
-  {
-    if(listBin[i] == "_")
-    {
-      bin += "0";
+    // remplace les caracteres par defaut par des 0
+    for (var i = 0; i < listBin.length; i++) {
+        if (listBin[i] == "_") {
+            bin += "0";
+        }
+        else {
+            bin += listBin[i];
+        }
     }
-    else
-    {
-      bin += listBin[i];
+
+    // affichage binaire
+    for (var i = 0; i < listBin.length; i++) {
+        document.getElementById('value' + i).innerHTML = (listBin[i]);
     }
-  }
 
-  // affichage binaire
-  for (var i = 0; i < listBin.length; i++)
-  {
-    document.getElementById('value'+i).innerHTML = (listBin[i]);
-  }
+    // calcul de l'hexadecimal
+    hex[0] = parseInt(bin.substring(0, 4), 2).toString(16).toUpperCase();
+    hex[1] = parseInt(bin.substring(4), 2).toString(16).toUpperCase();
 
-  // calcul de l'hexadecimal
-  hex[0] = parseInt(bin.substring(0,4),2).toString(16).toUpperCase();
-  hex[1] = parseInt(bin.substring(4),2).toString(16).toUpperCase();
-
-  // afiichage hexadecimal
-  for (var i = 0; i < hex.length; i++)
-  {
-    document.getElementById('hex'+i).innerHTML = hex[i];
-  }
-  // si la valeur calculee correspond a la valeur attendue
-  if (hex[0] == document.getElementById('sol0').innerHTML && hex[1] == document.getElementById('sol1').innerHTML )
-  {
-    Win();
-  }
+    // afiichage hexadecimal
+    for (var i = 0; i < hex.length; i++) {
+        document.getElementById('hex' + i).innerHTML = hex[i];
+    }
+    // si la valeur calculee correspond a la valeur attendue
+    if (hex[0] == document.getElementById('sol0').innerHTML && hex[1] == document.getElementById('sol1').innerHTML) {
+        Win();
+    }
 }
 
 // Finis la partie
-function Win(){
+function Win() {
 
-  var Address = "http://" + ADDR + "/escapeGame/end.php";
-  localStorage.clear();
-  alert("Gagné");
-  document.getElementById("TableSol1").hidden = true;
-  document.getElementById("winimage").hidden = false;
-  window.location.replace(Address);
+    var Address = "http://" + ADDR + "/end.php";
+    localStorage.clear();
+    console.log("GAGNER");
 }
+
+const req = new XMLHttpRequest();
+
+function checkServer() {
+
+    var  Address = "http://" + ADDR + "/soluce.php";
+    req.open('GET', Address, false);
+    req.send(null);
+
+    if (req.status === 200) {
+        var jsonData = JSON.parse(req.responseText);
+        var step1Validated = false;
+        var step2Validated = false;
+
+        if (jsonData.step1 != null)
+        {
+            step1Validated = true;
+            document.getElementById('sol0').innerHTML = jsonData.soluce1
+        }
+
+        if (jsonData.step2 != null)
+        {
+            step2Validated = true;
+            document.getElementById('sol1').innerHTML = jsonData.soluce2
+        }
+
+        if (step1Validated && step2Validated)
+        {
+            document.getElementById('B0').removeAttribute("disabled");
+            document.getElementById('B1').removeAttribute("disabled");
+            document.getElementById('B2').removeAttribute("disabled");
+        }
+    }
+}
+
+setInterval(checkServer, 2000);
